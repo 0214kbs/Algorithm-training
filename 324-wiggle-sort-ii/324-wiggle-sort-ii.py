fbdeb55ccbfdad1b5294b4fb0f3431227531834e
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2022-02-07 15:48:32 +0900

    Time: 250 ms (45.24%), Space: 17.1 MB (53.43%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+  
+        nums.sort()
+        #nums = [0,1,2,3,4,5]
+        
+        half = len(nums[::2])
+        #nums[::2] = [0,2,4]
+        #half = 3
+
+        nums[::2],nums[1::2] = nums[:half][::-1],nums[half:][::-1]
+        # nums[:half] = nums[:3] = [0,1,2]
+        # [::-1] means reverse = [2,1,0]
+        # nums = [2,1,1,3,0,5]
+        
+        # nums[1::2] = [1,3,5]
+        # nums[half:]=nums[3:] = [3,4,5]
+        # [::-1] means reverse = [5,4,3]
+        # nums = [2,5,1,4,0,3]
+        
