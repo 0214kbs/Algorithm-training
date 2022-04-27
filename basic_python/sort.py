875d849a117217934d19974ce5fe5181fd16f16e
bosunKwak <214kbs@gmail.com>
2022-04-27 22:21:23 +0900

    python - sort

---
+++ basic_python/sort.py
@@ -0,0 +1,15 @@
+# 오름차순으로 정렬
+a = [5,3,9,6,12,1]
+a.sort()
+print(a)
+
+# a를 변형시키지 않고 반환값으로 정렬
+a = [5,3,9,6,12,1]
+b = sorted(a)
+print(b)
+print(a)
+
+# 내림차순으로 정렬
+a.sort(reverse = True)
+print(a)
+
