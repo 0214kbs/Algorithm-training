b20fe3091be8e89c0252024bb95f5a2464bbe24f
곽보선 <87002218+bosunKwak@users.noreply.github.com>
2022-06-17 22:53:21 +0900

    Update CyclicRotation.py
    첫번째 시도에서 하나의 test case가 틀린 이유
     -> A가 empty인 경우를 고려해주어야 함!

--- codility/CyclicRotation/CyclicRotation.py
+++ codility/CyclicRotation/CyclicRotation.py
@@ -2,6 +2,8 @@ from collections import deque
 
 def solution(A, K):
     # write your code in Python 3.6
+    if len(A) == 0: #A가 empty인 경우를 고려해주어야 함! 
+        return A
     q = deque()
     for i in A:
         q.append(i)
