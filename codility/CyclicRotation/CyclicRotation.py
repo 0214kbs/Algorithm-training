3dfb6e60e4335e64f52e9067fc9a6546e3e57b40
곽보선 <87002218+bosunKwak@users.noreply.github.com>
2022-06-17 22:52:09 +0900

    Update CyclicRotation.py

--- codility/CyclicRotation/CyclicRotation.py
+++ codility/CyclicRotation/CyclicRotation.py
@@ -1,1 +1,18 @@
+from collections import deque
 
+def solution(A, K):
+    # write your code in Python 3.6
+    q = deque()
+    for i in A:
+        q.append(i)
+    # for i in range(K):
+    #     tmp = q.pop()
+    #     q.appendleft(tmp)
+    K = K%(len(A))
+    q.rotate(K)
+    # print(q)
+    for i in range(len(A)):
+        tmp = q.popleft()
+        A[i] = tmp
+    # print(A)
+    return A
