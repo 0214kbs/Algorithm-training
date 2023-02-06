024101f3bcb9c9d9c0facd33a8487d0e76f96cd7
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-02-06 22:00:28 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 364 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,12 +1,8 @@
-import sys
-
-
+# 실제 시험에서는 sys 사용 불가
 # 모든 구름이 di 방향으로 si칸 이동한다.
 # 각 구름에서 비가 내려 구름이 있는 칸의 바구니에 저장된 물의 양이 1 증가한다.
 def cloud_move(i, si):
     # cnt = 0 # 구름 개수
-    # print("clouds")
-    # print(clouds)
     for r in range(n):
         for c in range(n):
             if clouds[r][c] == 1:  # 구름이 있는경우
@@ -35,8 +31,6 @@ def cloud_move(i, si):
 # 2에서 물이 증가한 칸 (r, c)에 물복사버그 마법을 시전한다.
 # 물복사버그 마법을 사용하면, 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r, c)에 있는 바구니의 물이 양이 증가한다.
 def magic_copy():
-    # print("copy clouds")
-    # print(copy_clouds)
     for r in range(n):
         for c in range(n):
             sum = 0
@@ -62,7 +56,6 @@ def make_clouds():
                 maps[r][c] -= 2
 
 
-input = sys.stdin.readline
 maps = []
 clouds = []
 copy_clouds = []  # 물복사 마법을 위한 clouds
@@ -85,19 +78,14 @@ copy_clouds = [[0] * n for i in range(n)]
 
 for i in range(m):
     di, si = map(int, input().split())
-    # print("si")
-    # print(si)
     # 1,2
     cloud_move(di - 1, si)
-    # print(maps)
     # 3
     clouds = [[0] * n for j in range(n)]
     # 4
     magic_copy()
-    # print(maps)
     # 5
     make_clouds()
-    # print(maps)
     copy_clouds = [[0] * n for i in range(n)]
 
 water_sum = 0
