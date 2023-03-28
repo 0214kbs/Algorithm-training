90adb74489f994d751c07f58c399aac7727bb97d
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-28 16:57:45 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 17996 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main {
-	
+class Main {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = read();
+		int[] dp = new int[N + 1];
 		
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N+1];
-		if(N>1) {		
-			for(int i=2;i<N+1;i++) {
-				arr[i] = arr[i-1]+1;
-				if(i%3==0) arr[i] = Math.min(arr[i], arr[i/3]+1);
-				if(i%2==0) arr[i] = Math.min(arr[i], arr[i/2]+1);
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+			
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1); 
+			}
+			
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1); 
 			}
 		}
-		System.out.println(arr[N]);
 		
+		System.out.println(dp[N]);
+	}
+	
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
 		
+		while ((c = System.in.read()) > 32) {
+			n = (n << 3) + (n << 1) + (c & 15);
+		}
+		
+		return n;
 	}
 }
