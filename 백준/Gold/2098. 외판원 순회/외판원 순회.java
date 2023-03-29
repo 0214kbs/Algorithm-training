eed6a8a490518bfaac0eb5a06cff097506732f74
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-29 17:34:19 +0900

    [Gold I] Title: 외판원 순회, Time: 184 ms, Memory: 19020 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,33 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-public class Main {
-	private static int full, N, INF = 17 * 1000000 + 1;
+class Main {
+	
 	private static int[][] map, dp;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		String input[];
+	private static int full, N, INF = 17 * 1000000 + 1;
+	
+	public static void main(String[] args) throws Exception {
+		N = read();
 		map = new int[N][N];
-		full = (1 << N) - 1;
-		dp = new int[N][full];
+		
 		for (int i = 0; i < N; i++) {
-			input = br.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(input[j]);
-				if (map[i][j] == 0) map[i][j] = INF;
+				map[i][j] = read();
+				
+				if (map[i][j] == 0) {
+					map[i][j] = INF;
+				}
 			}
 		}
-		System.out.println(dfs(0, 1));
+		
+		full = (1 << N) - 1;
+		dp = new int[N][full];
+		
+		System.out.print(dfs(0, 1));
 	}
+	
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		
+		while ((c = System.in.read()) > 32) {
+			n = (n << 3) + (n << 1) + (c & 15);
+		}
+		
+		return n;
+	}
+	
 	private static int dfs(int curr, int visited) {
-		if (visited == full) return map[curr][0];
-		if (dp[curr][visited] != 0) return dp[curr][visited];
+		if (visited == full) {
+			return map[curr][0];
+		}
+		
+		if (dp[curr][visited] != 0) {
+			return dp[curr][visited];
+		}
+		
         int res = INF;
+        
 		for (int i = 0; i < N; i++) {
-			if (map[curr][i] == INF || (visited & (1 << i)) != 0) continue;
+			if (map[curr][i] == INF || (visited & (1 << i)) != 0) {
+				continue;
+			}
+			
 			res = Math.min(res, dfs(i, visited | (1 << i)) + map[curr][i]);
 		}
+		
 		return dp[curr][visited] = res;
 	}
 }
