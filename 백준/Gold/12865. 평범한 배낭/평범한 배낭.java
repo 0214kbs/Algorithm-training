11b980a9d30d871c42e9cb896c8fb360023de587
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-30 14:08:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,31 +1,29 @@
-class Main {
-	public static void main(String[] args) throws Exception {
-		int N = read();
-		int K = read();
-		
-		int[] dp = new int[K + 1];
-		
-		for (int i = 0; i < N; i++) {
-			int w = read();
-			int v = read();
-			
-			for (int j = K; j >= w; j--) {
-				if (dp[j] < dp[j - w] + v) {
-					dp[j] = dp[j - w] + v;
-				}
-			}
-		}
-		
-		System.out.print(dp[K]);
-	}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
 	
-	private static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		
-		while ((c = System.in.read()) > 32) {
-			n = (n << 3) + (n << 1) + (c & 15);
-		}
-		
-		return n;
-	}
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] dp = new int[K + 1];
+        
+        for (int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            for (int j = K; j >= w; j--) {
+                if (dp[j] < dp[j - w] + v) dp[j] = dp[j - w] + v;
+                
+            }
+        }
+        
+        System.out.print(dp[K]);
+    }
+    
 }
