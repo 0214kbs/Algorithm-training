709e5385dfdc629798418cbae3967a920ec7617e
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-30 17:13:37 +0900

    [Gold IV] Title: 연구소, Time: 492 ms, Memory: 51940 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,140 +1,114 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Main {
-	
-	private static class Point {
-		int r;
-		int c;
-		
-		public Point(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
+public class Main {
+
+	static int N, M, max;
+	static int[][] map, virusMap;
+	static boolean[][] visit;
+	static List<int[]> zero = new ArrayList<>(); // 벽을 세우는 조합의 src 해당
+	static List<int[]> virus = new ArrayList<>();
+	static int zeroSize;
+	static int[][] wall = new int[3][2]; // 벽이 세워지는 tgt 해당
+
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = {  0, 0,-1, 1 };
+
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+	    
+	    map = new int[N][M];
+	    max = Integer.MIN_VALUE;
+	    
+	    for (int i = 0; i < N; i++) {
+	        st = new StringTokenizer(br.readLine());
+	        for (int j = 0; j < M; j++) {
+	            int n = Integer.parseInt(st.nextToken());
+	            map[i][j] = n;
+	            
+	            if( n == 0 ) zero.add(new int[] {i, j});
+	            else if( n == 2 ) virus.add(new int[] {i, j});
+	        }
+	    }
+	    
+	    zeroSize = zero.size();
+	    
+	    // 벽 3개를 조합으로 처리
+	    comb( 0, 0 );
+	    
+	    System.out.println(max);
 	}
-	
-	private static int[] dr = {-1, 1, 0, 0};
-	private static int[] dc = {0, 0, -1, 1};
-	
-	private static Queue<Point> queue;
-	private static Point[] points;
-	private static boolean[][] visited;
-	private static int[][] map;
-	private static int[][] copy;
-	private static int N, M, answer;
-	
-	public static void main(String[] args) throws Exception {
-		answer = 0;
-		N = read();
-		M = read();
-		
-		points = new Point[N * M];
-		map = new int[N][M];
-		
-		int index = 0;
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				points[index++] = new Point(i, j);
-				map[i][j] = read();
-			}
-		}
-		
-		queue = new ArrayDeque<>();
-		
-		for (int i = 0; i < N * M; i++) {
-			if (map[points[i].r][points[i].c] != 0) {
-				continue;
-			}
-			
-			map[points[i].r][points[i].c] = 1;
-			
-			for (int j = i + 1; j < N * M; j++) {
-				if (map[points[j].r][points[j].c] != 0) {
-					continue;
-				}
-				
-				map[points[j].r][points[j].c] = 1;
-				
-				for (int k = j + 1; k < N * M; k++) {
-					if (map[points[k].r][points[k].c] != 0) {
-						continue;
-					}
-					
-					map[points[k].r][points[k].c] = 1;
-					
-					visited = new boolean[N][M];
-					copy = new int[N][M];
-					
-					for (int r = 0; r < N; r++) {
-						for (int c = 0; c < M; c++) {
-							copy[r][c] = map[r][c];
-						}
-					}
-					
-					for (int r = 0; r < N; r++) {
-						for (int c = 0; c < M; c++) {
-							if (map[r][c] == 2 && !visited[r][c]) {
-								bfs(r, c);
-							}
-						}
-					}
-					
-					int count = 0;
-					
-					for (int r = 0; r < N; r++) {
-						for (int c = 0; c < M; c++) {
-							if (copy[r][c] == 0) {
-								count++;
-							}
-						}
-					}
-					
-					if (count > answer) {
-						answer = count;
-					}
-					
-					map[points[k].r][points[k].c] = 0;
-				}
-				
-				map[points[j].r][points[j].c] = 0;
-			}
-			
-			map[points[i].r][points[i].c] = 0;
-		}
-		
-		System.out.println(answer);
+
+	static void comb(int srcIdx, int tgtIdx) {
+	    // 기저조건
+	    if( tgtIdx == 3 ) {
+	        // complete code
+	        // 벽을 다 세운 상태
+	        virus();
+	        return;
+	    }
+	    
+	    if( srcIdx == zeroSize ) return;
+	    
+	    wall[tgtIdx] = zero.get(srcIdx);
+	    
+	    comb(srcIdx + 1, tgtIdx + 1);// 현재 srcIdx 선택
+	    comb(srcIdx + 1, tgtIdx);// 현재 srcIdx 선택 X 같은 tgtIdx 에 다음 srcIdx 를  고려
+	    
 	}
-	
-	private static int read() throws Exception {
-    	int c, n = System.in.read() & 15;
-    	
-    	while ((c = System.in.read()) > 32) {
-    		n = (n << 3) + (n << 1) + (c & 15);
-    	}
-    	
-    	return n;
-    }
-	
-	private static void bfs(int r, int c) {
-		queue.offer(new Point(r, c));
-		visited[r][c] = true;
-		
-		while (!queue.isEmpty()) {
-			Point curr = queue.poll();
-			
-			for (int i = 0; i < 4; i++) {
-				int nr = curr.r + dr[i];
-				int nc = curr.c + dc[i];
-				
-				if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc] || copy[nr][nc] == 1) {
-					continue;
-				}
-				
-				queue.offer(new Point(nr, nc));
-				visited[nr][nc] = true;
-				copy[nr][nc] = 2;
-			}
-		}
+
+
+	static void virus() {
+	    // virus 확산
+	    // 안전지대 계산
+	    
+	    virusMap = new int[N][M];
+	    visit = new boolean[N][M];
+	    
+	    // map copy
+	    for (int i = 0; i < N; i++) {
+	        for (int j = 0; j < M; j++) {
+	            virusMap[i][j] = map[i][j];
+	        }
+	    }
+	    
+	    // virusMap 에 벽을 세운다
+	    for (int i = 0; i < 3; i++) {
+	        virusMap[ wall[i][0] ][ wall[i][1] ] = 1;
+	    }
+	    
+	    // virus 확산
+	    for (int[] v : virus) {
+	        dfs( v[0], v[1] );
+	    }
+	    
+	    // 안전 지대 계산 max 갱신
+	    int sum = 0;
+	    for (int i = 0; i < N; i++) {
+	        for (int j = 0; j < M; j++) {
+	            if( virusMap[i][j] == 0 ) sum++;
+	        }
+	    }
+	    
+	    max = Math.max(max, sum);
+	}
+
+	static void dfs( int y, int x ) {
+	    for (int d = 0; d < 4; d++) {    
+	        int ny = y + dy[d];
+	        int nx = x + dx[d];
+	        if( ny < 0 || nx < 0 || ny >= N || nx >= M || visit[ny][nx] ) continue;
+	        if( virusMap[ny][nx] == 0 ) {
+	            virusMap[ny][nx] = 2;
+	            visit[ny][nx] = true;
+	            dfs( ny, nx);
+	        }
+	    }
 	}
 }
