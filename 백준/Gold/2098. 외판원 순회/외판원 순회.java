96ff28d4a80f6c874a3873ded90cf480ed733a9f
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-30 12:11:05 +0900

    [Gold I] Title: 외판원 순회, Time: 184 ms, Memory: 19820 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,57 +1,60 @@
-class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
 	
-	private static int[][] map, dp;
-	private static int full, N, INF = 17 * 1000000 + 1;
+	static int N, allMask, INF = 987654321;
+	static int[][] W;
+	static int[][] dp;
 	
-	public static void main(String[] args) throws Exception {
-		N = read();
-		map = new int[N][N];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 		
+		// 자료구조 
+		allMask = (1<<N)-1;
+		// ex 5개 도시 : 11111  
+		//=> 1 << 5 (= 100000) -1 (= 11111) 
+		W = new int[N][N];
+		dp = new int[N][allMask];
+		
+		// 입력
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				map[i][j] = read();
-				
-				if (map[i][j] == 0) {
-					map[i][j] = INF;
-				}
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				W[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		full = (1 << N) - 1;
-		dp = new int[N][full];
-		
-		System.out.print(dfs(0, 1));
+		// 풀이
+		System.out.println(tsp(0,1));
 	}
 	
-	private static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		
-		while ((c = System.in.read()) > 32) {
-			n = (n << 3) + (n << 1) + (c & 15);
+	// idx 도시부터 아직 방문하지 않은 남은 모든 도시를 방문하는 최소 비용을 return 
+	static int tsp(int idx, int mask) {
+		// 기저 조건
+		// 모든 도시를 방문 => 돌아가는 비용 확인
+		if(mask == allMask) {
+			if(W[idx][0] == 0) return INF; // 처음으로 되돌아 가지 못하는 경우
+			else return W[idx][0]; // 처음으로 되돌아가는 비용 리턴 
 		}
 		
-		return n;
-	}
-	
-	private static int dfs(int curr, int visited) {
-		if (visited == full) {
-			return map[curr][0];
-		}
+		// 이미 이전에 계산된 값이 존재
+		if(dp[idx][mask]!=0) return dp[idx][mask];
 		
-		if (dp[curr][visited] != 0) {
-			return dp[curr][visited];
-		}
 		
-        int res = INF;
-        
-		for (int i = 0; i < N; i++) {
-			if (map[curr][i] == INF || (visited & (1 << i)) != 0) {
-				continue;
-			}
+		// 최소비용 INF 초기화
+		dp[idx][mask] = INF;
+		// 남은 도시를 모두 방문하는 최소 비용을 계산
+		for(int i=0;i<N;i++) {
+			// 갈 수 있거나, 이미 방문했으면 skip
+			if(W[idx][i]==0 || (mask & (1<<i)) != 0) continue; 
+			// 남은 i를 거치는 비용과 현재 비용을 비교하면서 최소 값을 갱신
+			dp[idx][mask] = Math.min(dp[idx][mask], tsp(i,mask|(1<<i))+W[idx][i]); // mask에 i번째도 간다라는 표시
 			
-			res = Math.min(res, dfs(i, visited | (1 << i)) + map[curr][i]);
 		}
+		return dp[idx][mask];
 		
-		return dp[curr][visited] = res;
 	}
 }
