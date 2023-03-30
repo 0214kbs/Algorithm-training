cfd2e1a18e698c8e960493ce443fc212f62e2bb0
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-30 17:05:21 +0900

    [Gold IV] Title: 불!, Time: 372 ms, Memory: 39600 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -17,55 +17,57 @@ class Main{
 		}
 	}
 	
+	private static int[] dr = {-1, 1, 0, 0};
+	private static int[] dc = {0, 0, -1, 1};
+	
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int startX,startY;
     public static void main(String[] args)throws Exception{
-        ArrayDeque<Point> q = new ArrayDeque<>();
+        ArrayDeque<Point> queue = new ArrayDeque<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[][] dxy = {{0,1},{0,-1},{1,0},{-1,0}};
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        char[][]map = new char[x][y];
-        for(int i=0; i<x;i++)
-            for(int j=0; j<y; j++){
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        char[][]map = new char[R][C];
+        for(int i=0; i<R;i++)
+            for(int j=0; j<C; j++){
             	map[i][j] = read();
                 if(map[i][j]=='J'){
                     startX = i;
                     startY = j;
                 }else if(map[i][j]=='F'){
-                    q.add(new Point(i, j, 0));
+                	queue.add(new Point(i, j, 0));
                 }
             }
         
-        q.add(new Point(startX, startY, 0));
+        queue.add(new Point(startX, startY, 0));
         
-        while(!q.isEmpty()){
-            Point curr = q.poll();
-            
-            for(int[] xy : dxy){
-                int nr = curr.r + xy[0];
-                int nc = curr.c + xy[1];
-                int dist = curr.turn + 1;
-                
-                if (nr < 0 || nr >= x || nc < 0 || nc >= y) {
+        while (!queue.isEmpty()) {
+			Point curr = queue.poll();
+				
+			for (int i = 0; i < 4; i++) {
+				int nr = curr.r + dr[i];
+				int nc = curr.c + dc[i];
+					
+				if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
 					if (map[curr.r][curr.c] == 'J') {
-						System.out.println(dist);
+						System.out.println(curr.turn + 1);
 						return;
 					}
 					
 					continue;
 				}
-
-                if (map[nr][nc] != '.') {
+				
+				if (map[nr][nc] != '.') {
 					continue;
 				}
-                
-                map[nr][nc] = map[curr.r][curr.c];
-                q.add(new Point(nr,nc,dist));
-                
-            }
-        }
-        System.out.println("IMPOSSIBLE");
+					
+				queue.offer(new Point(nr, nc, curr.turn + 1));
+				map[nr][nc] = map[curr.r][curr.c];
+			}
+		}
+		
+		System.out.print("IMPOSSIBLE");
     }
     public static char read()throws Exception{
         while(true){
