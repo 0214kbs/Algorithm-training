cf9eadaf97a345b2561fd3021914063059f7f90c
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-03-31 13:49:44 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 340 ms, Memory: 22240 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,60 @@
+class Main {
+	
+	private static int[] arr, dp;
+	private static int N, answer;
+	
+	public static void main(String[] args) throws Exception {
+		N = read();
+		arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = read();
+		}
+		
+		dp = new int[N];
+		dp[0] = arr[0];
+		
+		for (int i = 1; i < N; i++) {
+			if (dp[answer] < arr[i]) {
+				dp[++answer] = arr[i];
+				
+			} else {
+				binarySearch(arr[i]);
+			}
+		}
+		
+		System.out.println(answer + 1);
+	}
+	
+	private static int read() throws Exception {
+    	int c, n = System.in.read() & 15;
+    	
+    	while ((c = System.in.read()) > 32) {
+    		n = (n << 3) + (n << 1) + (c & 15);
+    	}
+    	
+    	return n;
+    }
+	
+	private static void binarySearch(int num) {
+		int start = 0;
+		int end = answer;
+		
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			
+			if (dp[mid] < num) {
+				start = mid + 1;
+				
+			} else {
+				if (dp[mid] == num) {
+					return;
+				}
+				
+				end = mid - 1;
+			}
+		}
+		
+		dp[start] = num;
+	}
+}
