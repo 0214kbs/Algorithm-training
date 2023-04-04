05ea6174afc4332dd22e37db576715802b8cd48c
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-04-04 14:01:24 +0900

    [Platinum V] Title: 찾기, Time: 524 ms, Memory: 39676 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,48 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		char[] text = in.readLine().toCharArray();
 		char[] pattern = in.readLine().toCharArray();
 		
 		int tLength = text.length, pLength = pattern.length;
+		
+		// 부분일치테이블 만들기 : KMP의 아이디어를 똑같이 적용, W에서 W를 찾는 듯한 행위를 해서...
 		int[] pi = new int[pLength];
-		
-	    for (int i = 1, j = 0; i < pLength; i++) {
-	        while (j > 0 && pattern[i] != pattern[j]) {
-	        	j = pi[j - 1]; 
-	        }
+	    for(int i=1, j=0; i<pLength; i++){// i:접미사 포인터(i=1부터 시작: 우리는 부분일치테이블를 만드는게 목적이므로 첫글자 틀리면 0위치로 가야하므로.), j:접두사 포인터
+	        while(j > 0 && pattern[i] != pattern[j]) j = pi[j-1]; 
 	        
-	        if (pattern[i] == pattern[j]) {
-	        	pi[i] = ++j;
-	        	
-	        } else {
-	        	pi[i] = 0;
-	        }
+	        if(pattern[i] == pattern[j]) pi[i] = ++j;
+	        else pi[i] = 0;
 	    }
-	    
 	    StringBuilder sb = new StringBuilder();
-		int cnt = 0;
 		
-		for (int i = 0, j = 0; i < tLength; i++) {
-			while (j > 0 && text[i] != pattern[j]) {
-				j = pi[j - 1]; 
-			}
+		int cnt = 0;
+		// i : 텍스트 포인터 , j: 패턴 포인터 
+		for(int i=0,j=0; i<tLength; ++i) { 
 			
-			if (text[i] == pattern[j]) {
-				if (j == pLength - 1) {
-					cnt++;
-					sb.append(i - pLength + 2).append('\n');  
-					j = pi[j]; 
-					
-				} else { 
+			while(j>0 && text[i] != pattern[j]) j = pi[j-1]; 
+			
+			if(text[i] == pattern[j]) { //두 글자 일치
+				if(j == pLength-1) { // j가 패턴의 마지막 인덱스라면 
+					cnt++; // 카운트 증가
+					sb.append(i-pLength+2).append('\n');  
+					j=pi[j]; 
+				}else { 
 					j++;
 				}
 			}
 		}
-		
 		System.out.println(cnt);
 		System.out.println(sb);
 	}
