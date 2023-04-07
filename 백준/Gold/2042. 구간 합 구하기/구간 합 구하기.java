b57da05b2f9de63d374bb7fca220674f5973a1da
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-04-07 16:10:13 +0900

    [Gold I] Title: 구간 합 구하기, Time: 800 ms, Memory: 103732 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,85 +1,68 @@
-class Main {
-	
-	private static long[] bit, arr;
-	private static int N, MK;
-	
-	public static void main(String[] args) throws Exception {
-		N = readInt();
-		MK = readInt() + readInt();
-		
-		bit = new long[N + 1];
-		arr = new long[N + 1];
-		
-		for (int i = 1; i <= N; i++) {
-			update(i, readLong());
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		while (MK-- > 0) {
-			switch (readInt()) {
-				case 1: update(readInt(), readLong()); break;
-				case 2: sb.append(query(readInt(), readInt())).append('\n'); break;
-			}
-		}
-		
-		System.out.print(sb);
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K;
+	static long[] nums, tree;
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+	    K = Integer.parseInt(st.nextToken());
+
+	    nums = new long[N + 1]; // 0 dummy
+	    tree = new long[N + 1]; // 0 dummy
+	    
+	    for (int i = 1; i <= N; i++) {
+	        st = new StringTokenizer(br.readLine());
+	        nums[i] = Long.parseLong(st.nextToken());
+	    }
+	    
+	    // 펜윅 트리 구성
+	    for (int i = 1; i <= N; i++) {
+	        update( i, nums[i]);// 최소 tree 생성시에는 배열이 0 이므로 값을 그대로 전달하면 그것이 diff
+	    }
+	    
+	    // 변경 또는 합
+	    for (int i = 0; i < M + K; i++) {
+	        st = new StringTokenizer(br.readLine());
+	        int a = Integer.parseInt(st.nextToken());
+	        
+	        if( a == 1 ) { // b 번째 수를 c 로 변경
+	            int b = Integer.parseInt(st.nextToken());
+	            long c = Long.parseLong(st.nextToken());
+	            long temp = nums[b];
+	            nums[b] = c; // 원본 배열 의 b 자리를 c 로 변경
+	            update( b, c - temp );
+	        }else if( a == 2 ) { // b 번째 ~ c 번째 수의 합
+	            int b = Integer.parseInt(st.nextToken());
+	            int c = Integer.parseInt(st.nextToken());
+	            long s = sum(b, c);
+	            System.out.println(s);
+	        }
+	    }
 	}
-	
-	private static int readInt() throws Exception {
-		int c, n = System.in.read() & 15;
-		boolean isNegative = n == 13;
-		
-		if (isNegative) {
-			n = System.in.read() & 15;
-		}
-		
-		while ((c = System.in.read()) > 32) {
-			n = (n << 3) + (n << 1) + (c & 15);
-		}
-		
-		return isNegative ? ~n + 1 : n;
+
+	static void update(int i, long diff) {
+	    while( i < N + 1 ){
+	        tree[i] += diff;
+	        i += ( i & -i);
+	    }
 	}
-	
-	private static long readLong() throws Exception {
-		long n = System.in.read() & 15;
-		int c;
-		
-		boolean isNegative = n == 13;
-		
-		if (isNegative) {
-			n = System.in.read() & 15;
-		}
-		
-		while ((c = System.in.read()) > 32) {
-			n = (n << 3) + (n << 1) + (c & 15);
-		}
-		
-		return isNegative ? ~n + 1 : n;
+
+	static long sum(int i) {
+	    long result = 0;
+	    while( i > 0 ) {
+	        result += tree[i];
+	        i -= ( i & -i );
+	    }
+	    return result;        
 	}
-	
-	private static long query(int b, int c) {
-		return getPrefixSum(c) - getPrefixSum(b - 1);
-	}
-	
-	private static long getPrefixSum(int target) {
-		long answer = 0;
-		
-		while (target > 0) {
-			answer += bit[target];
-			target -= target & -target;
-		}
-		
-		return answer;
-	}
-	
-	private static void update(int target, long value) {
-		long diff = value - arr[target];
-		arr[target] = value;
-		
-		while (target <= N) {
-			bit[target] += diff;
-			target += target & -target;
-		}
+
+	static long sum(int i, int j) {
+	    return sum(j) - sum(i-1);
 	}
 }
