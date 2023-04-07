9dd649f507f868ceef4f0b86f164b2365b5c802d
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-04-07 09:10:09 +0900

    [Silver V] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 152 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -0,0 +1,42 @@
+class Main {
+	public static void main(String[] args) throws Exception {
+		int N = read();
+		int M = read();
+		
+		boolean[][] arr = new boolean[N + 1][N + 1];
+		
+		while (M-- > 0) {
+			int a = read();
+			int b = read();
+			
+			arr[a][b] = true;
+			arr[b][a] = true;
+		}
+		
+		int answer = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = i + 1; j <= N; j++) {
+				for (int k = j + 1; k <= N; k++) {
+					if (arr[i][j] || arr[j][k] || arr[k][i]) {
+						continue;
+					}
+					
+					answer++;
+				}
+			}
+		}
+		
+		System.out.print(answer);
+	}
+	
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		
+		while ((c = System.in.read()) > 32) {
+			n = (n << 3) + (n << 1) + (c & 15);
+		}
+		
+		return n;
+	}
+}
