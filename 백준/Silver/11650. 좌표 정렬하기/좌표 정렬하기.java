a156c7851d19c122a231daa129e19314bea51aba
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-05-28 19:40:56 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 716 ms, Memory: 49164 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -11,35 +11,36 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		Pair[] arr = new Pair[N];
-		
-		for(int i=0;i<N;i++) {
+
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			arr[i] = new Pair(x,y);
+			arr[i] = new Pair(x, y);
 		}
-		
-		Arrays.sort(arr, new Comparator<Pair>() {
-			@Override
-			public int compare(Pair p1, Pair p2) {
-				if(p1.x == p2.x) return p1.y-p2.y;
-				else return p1.x-p2.x;	
-			}
+
+		Arrays.sort(arr, (p1, p2) -> {
+			if (p1.x == p2.x) return p1.y - p2.y;
+			else return p1.x - p2.x;
 		});
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			sb.append(arr[i].x);
 			sb.append(" ");
 			sb.append(arr[i].y);
 			sb.append("\n");
-		}		
+		}
+
 		System.out.println(sb);
 	}
-	
-	static class Pair{
+
+	static class Pair {
 		int x, y;
-		public Pair() {}
+
+		public Pair() {
+		}
+
 		public Pair(int x, int y) {
 			super();
 			this.x = x;
