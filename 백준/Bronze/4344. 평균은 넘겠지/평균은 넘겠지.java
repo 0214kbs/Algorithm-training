0b0a526bb3ab533e6f5761270672280c7bd9fc2d
곽보선 <87002218+0214kbs@users.noreply.github.com>
2023-07-03 22:35:19 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 272 ms, Memory: 28136 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int overAvg =0;
+        int n = sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int sum=0, cnt=0;
-		int c =sc.nextInt();
-		for(int i=0;i<c;i++) {
-			int n = sc.nextInt();
-			int[] score= new int[n];
-			for(int j=0;j<n;j++) {
-				score[j]=sc.nextInt();
-				sum+=score[j];
-			}
-			double avr = sum/n;		
-			for(int j=0;j<n;j++) {
-				if(score[j]>avr)cnt++;
-			}
-			System.out.print(String.format("%.3f",(float)cnt/n*100));
-			System.out.println("%");
-			cnt=0;sum=0;
-		}
-		
-		sc.close();
-	}
+        for (int i = 0;i<n;i++){
+            int cnt = sc.nextInt();           
+            int[] score = new int[cnt];
 
+            for(int j =0;j<cnt;j++){
+                score[j] = sc.nextInt();
+                sum += score[j];
+            }
+            double avr = sum/cnt;
+            for(int j =0;j<cnt;j++){
+                if(avr<score[j]){
+                    overAvg ++;
+                }
+            }
+            System.out.println((String.format("%.3f",(float)overAvg/cnt*100)+"%"));
+            sum =0;
+            overAvg =0;
+        }
+        
+        sc.close();
+    }
 }
