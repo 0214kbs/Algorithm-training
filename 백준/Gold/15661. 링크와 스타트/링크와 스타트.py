f3cb049be9c40516e377d4da9a68095d996ad497
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-01-19 18:49:01 +0900

    [Gold V] Title: 링크와 스타트, Time: 4432 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
@@ -1,5 +1,5 @@
+import sys
 from itertools import combinations
-
 
 def calc_score(S):
     global mat
@@ -8,28 +8,18 @@ def calc_score(S):
         ret += mat[i][j]+mat[j][i]
     return ret
 
-def dfs(L, BW, current_set, remaining_set):
-    global ans
-    if L == r:
-        a = calc_score(current_set)
-        b = calc_score(remaining_set - current_set)
-        ret = abs(a - b)
-        if ans > ret:
-            ans = ret
-    else:
-        for i in range(BW, len(n)):
-            res[L] = n[i]
-            dfs(L + 1, i + 1, current_set | {n[i]}, remaining_set)
-
-ans = 1e9
+ans = 1e5
 N = int(input())
 S = set(range(N))
 mat = [list(map(int, input().split())) for _ in range(N)]
-
-for cnt in range(1, N//2 + 1):
-    n = list(range(N))
-    r = cnt
-    res = [0] * r
-    dfs(0, 0, set(), S)
+for cnt in range(1, N//2+1):
+    for comb in combinations(list(range(N)), cnt):
+        C = set(comb)
+        a = calc_score(C)
+        b = calc_score(S-C)
+        ret = abs(a-b)
+        if ans > ret:
+            ans = ret
+            if not ans: break
 
 print(ans)
