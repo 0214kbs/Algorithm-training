813e7c77cd8b7a8394cdef51a04a74343f83a7fb
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-02-10 23:13:52 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 564 ms, Memory: 306140 KB -BaekjoonHub

---
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = [[0]*(n+1) for i in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if nums[-i]==nums[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+print(n-dp[n][n])
