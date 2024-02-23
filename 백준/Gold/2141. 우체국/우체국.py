9ba2ce28f7440e386f9f445b0ac08a8824e39998
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-02-23 16:01:53 +0900

    [Gold IV] Title: 우체국, Time: 248 ms, Memory: 122260 KB -BaekjoonHub

---
+++ 백준/Gold/2141. 우체국/우체국.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+xalist = []
+pos = 0
+for i in range(n):
+    x,a = map(int, input().split())
+    xalist.append([x,a])
+    pos += a
+
+xalist.sort(key=lambda x:x[0])
+
+cnt = 0
+for i in range(n):
+    cnt += xalist[i][1]
+    if cnt >= pos/2:
+        print(xalist[i][0])
+        break
