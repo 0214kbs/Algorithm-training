0b11fd55b16ee6d46f46b8c4bb40f8f1e56c0e67
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-04-03 16:44:02 +0900

    [Gold IV] Title: LCS 3, Time: 188 ms, Memory: 120052 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -4,7 +4,6 @@ A = input().rstrip()
 B = input().rstrip()
 C = input().rstrip()
 
-res = 0
 LCS = [[[0] * (len(C)+1) for _ in range(len(B)+1)] for _ in range(len(A)+1) ]
 for i in range(1, len(A)+1):
     for j in range(1, len(B)+1):
@@ -13,6 +12,5 @@ for i in range(1, len(A)+1):
                 LCS[i][j][k] = LCS[i-1][j-1][k-1]+1
             else:
                 LCS[i][j][k] = max(LCS[i-1][j][k], LCS[i][j-1][k], LCS[i][j][k-1])
-            res = max(LCS[i][j][k],res)
 
-print(res)
+print(LCS[-1][-1][-1])
