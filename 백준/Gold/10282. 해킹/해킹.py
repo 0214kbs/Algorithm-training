85b4993ef49dca1198bc0fef150955e5284e3646
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-07-09 14:01:26 +0900

    [Gold IV] Title: 해킹, Time: 696 ms, Memory: 166776 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,36 +1,31 @@
 import sys,heapq
-input =sys.stdin.readline
-
-INF = float('inf')
+input = sys.stdin.readline
+INF = int(1e9)
 T = int(input())
 for _ in range(T):
     n, d, c = map(int, input().split())
-    deps = [[] for _ in range(n + 1)]
+    deps = [[] for _ in range(n+1)]
     for _ in range(d):
-        a, b, s = map(int, input().split())
-        deps[b].append([a, s])
+        a,b,s = map(int, input().split())
+        deps[b].append([s,a])
 
-    heap = []
-    dist = [INF for _ in range(n + 1)]
-    visited = [0 for _ in range(n + 1)]
-    dist[c] = 0
-    heapq.heappush(heap, [0, c])
-
-    while heap:
-        cost, node = heapq.heappop(heap)
-        if visited[node] == 1:
-            continue
-        visited[node] = 1
-        for nxt, d in deps[node]:
-            if not visited[nxt]:
-                if dist[nxt] > cost + d:
-                    dist[nxt] = cost + d
-                    heapq.heappush(heap, [dist[nxt], nxt])
-
-    count, time = 0, 0
-    for c in range(1, n + 1):
-        if visited[c]:
-            count += 1
-            time = max(time, dist[c])
-
-    print(count, time)
+    hq = []
+    dp = [INF for _ in range(n+1)]
+    heapq.heappush(hq,[0, c])
+    dp[c] = 0
+    # print(dp)
+    while hq:
+        time, cur = heapq.heappop(hq)
+        # print(time,cur)
+        for nxt in deps[cur]:
+            nxt_time = time+nxt[0]
+            if nxt_time < dp[nxt[1]]:
+                dp[nxt[1]] = nxt_time
+                heapq.heappush(hq,[nxt_time, nxt[1]])
+    # print(dp)
+    res = [0,0]
+    for i in range(1,n+1):
+        if dp[i] < INF:
+            res[1] = max(res[1],dp[i])
+            res[0] += 1
+    print(*res)
