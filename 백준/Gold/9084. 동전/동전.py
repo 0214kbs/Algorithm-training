821e10b05abfdb663c507234d37341388f126a69
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-08-18 20:47:32 +0900

    [Gold V] Title: 동전, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -2,15 +2,18 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-for t in range(T):
+for _ in range(T):
     N = int(input())
     coins = list(map(int, input().split()))
-    want = int(input())
-    dp = [0]*(want+1)
-    dp[0] = 1
+    M = int(input())
 
-    coins.sort()
+    dp = [0 for _ in range(M+1)]
     for c in coins:
-        for i in range(c,want+1):
-            dp[i] += dp[i-c]
-    print(dp[want])
+        if c>M:
+            continue
+        dp[c] += 1
+        for i in range(M+1):
+            if M+1>(i-c)>0:
+                dp[i] = dp[i]+dp[i-c]
+
+    print(dp[M])
