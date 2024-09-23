de2375e6c3381c2334975d3ded8f91a319141cc6
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-09-23 16:21:50 +0900

    [Gold V] Title: 토마토, Time: 460 ms, Memory: 203996 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,45 +1,36 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
-dx = [1, -1, 0, 0, 0, 0]
-dy = [0, 0, 1, -1, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
 
+# 위, 아래, 왼쪽, 오른쪽, 앞, 뒤
+drc = [(-1,0,0),(1,0,0),(0,-1,0), (0,1,0),(0,0,-1),(0,0,1)]
+M, N, H = map(int, input().split())
+storage = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
-def bfs():
-    while q:
-        x, y, z = q.popleft()
-        for i in range(6):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            nz = z + dz[i]
-            if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and maps[nx][ny][nz] == 0:
-                maps[nx][ny][nz] = maps[x][y][z] + 1
-                q.append([nx, ny, nz])
-
-
-m, n, h = map(int, input().split())
-maps = []
+zero = 0
 q = deque()
+for k in range(H):
+    for i in range(N):
+        for j in range(M):
+            if storage[k][i][j] == 1:
+                q.append([0, k, i, j])
+            elif storage[k][i][j] == 0:
+                zero+=1
 
-for i in range(h):
-    boxes = []
-    for j in range(n):  # 익은 토마토 저장
-        boxes.append(list(map(int, input().split())))
-        for k in range(m):
-            if boxes[j][k] == 1:
-                q.append([i, j, k])
-    maps.append(boxes)
+time = 0
+while q:
+    ct, ch, cr,cc = q.popleft()
+    for dh, dr,dc in drc:
+        nh, nr,nc = ch+dh, cr+dr, cc+dc
+        if 0<=nr<N and 0<=nc<M and 0<=nh<H:
+            if storage[nh][nr][nc] == 0:
+                zero -=1
+                storage[nh][nr][nc] = ct+1
+                q.append([ct+1, nh, nr,nc])
+                time = max(ct+1, time)
 
-bfs()
-
-res = 0
-for i in maps:
-    for j in i:
-        for k in j:
-            if k == 0:
-                print(-1)
-                exit(0)
-        res = max(res, max(j))
-print(res - 1)
+# print(storage)
+if zero == 0:
+    print(time)
+else:
+    print(-1)
