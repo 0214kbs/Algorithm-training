b2bc1ef28a5d7d193320776ddbab03d6652938bd
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-10-11 23:58:40 +0900

    [Gold IV] Title: 여행 가자, Time: 112 ms, Memory: 114000 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,47 +1,41 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n= int(input())
-m =int(input())
 
-def find(i):
-    if parent[i] == i:
-        return i
-    else:
-        parent[i] = parent[parent[i]]
-        return parent[i]
+N = int(input())
+M = int(input())
 
-def union(i,j):
-    a = find(i)
-    b = find(j)
+sets = [0 for _ in range(N+1)]
+graph = {i:[]for i in range(1,N+1)}
+for i in range(1,N+1):
+    tmp = list(map(int, input().split()))
+    for j in range(1,N+1):
+        if tmp[j-1] == 1:
+            graph[i].append(j)
+# print(graph)
+cur_set = 1
+for i in range(1,N+1):
+    if sets[i] > 0:
+        continue
+    # sets 체크
+    q = deque()
+    q.append(i)
+    sets[i] = cur_set
+    while q:
+        cur = q.popleft()
+        for nxt in graph[cur]:
+            if sets[nxt] == 0:
+                q.append(nxt)
+                sets[nxt] = cur_set
+    cur_set += 1
 
-    if a == b:
-        return
-    elif a<b:
-        for idx in range(n+1):
-            if parent[idx] == a:
-                parent[idx] = b
-    else:
-        parent[b] = a
-
-parent = [i for i in range(n+1)]
-
-for i in range(1,n+1):
-    jlist = list(map(int, input().split()))
-    for j in range(n):
-        if jlist[j] == 1:
-            union(min(i,j+1),max(i,j+1))
-
-
-quest = list(map(int, input().split()))
-tmp = parent[quest[0]]
-flag = True
-for q in range(1, len(quest)):
-    if parent[quest[q]] != tmp:
-        flag = False
-        break
-# print(parent)
-if flag:
-    print('YES')
+# print(sets[1:])
+travels = list(map(int, input().split()))
+res = set()
+for t in travels:
+    res.add(sets[t])
+if len(res) == 1:
+    print("YES")
 else:
-    print('NO')
+    print("NO")
