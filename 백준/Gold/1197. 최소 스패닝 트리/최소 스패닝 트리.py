bc758799553b11fa9217d8bcb6534dfe7703a6dc
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-10-13 12:02:03 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 412 ms, Memory: 54676 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,33 @@
 import sys, heapq
-
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
-v, e= map(int, input().split())
-visit = [False]*(v+1)
-trees = {}
-for i in range(1,v+1):
-    trees[i] = []
-for _ in range(e):
+def find(parent, a):
+    if parent[a] != a:
+        parent[a] = find(parent, parent[a])
+    return parent[a]
+
+def union(parent, a,b):
+    a = find(parent,a)
+    b = find(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+V, E = map(int, input().split())
+parent = [ i for i in range(V+1)]
+heap = []
+min_cost = 0
+for _ in range(E):
     a,b,c = map(int, input().split())
-    trees[a].append([c,b])
-    trees[b].append([c,a])
+    heapq.heappush(heap, [c,a,b])
 
-cnt = 0
-res = 0
-q = []
-heapq.heappush(q,[0,1])
-while q:
-    if cnt == v:
-        break
-    val, nxt = heapq.heappop(q)
-    if not visit[nxt]:
-        visit[nxt] = True
-        cnt += 1
-        res += val
-        for i in trees[nxt]:
-            heapq.heappush(q,i)
+while heap:
+    cost, a,b = heapq.heappop(heap)
 
-print(res)
+    if find(parent,a) != find(parent,b):
+        union(parent,a,b)
+        min_cost += cost # 간선 비용
+print(min_cost)
